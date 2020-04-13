11690e1c5cc435c1e5000ae52f959bb03e108ec3
Yinevg <864248425@qq.com>
2020-04-13 23:56:08 +0800

    add mybatis generator

---
+++ mybatis-demo/src/main/java/com/demo/MybatisMain.java
@@ -0,0 +1,7 @@
+package com.demo;
+
+/**
+ * Created by Yinevg on 2020/4/13
+ */
+public class MybatisMain {
+}
---
+++ mybatis-demo/src/main/resources/init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `mybatis_demo`.`mybatis_test` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `key` VARCHAR(45) NOT NULL DEFAULT '',
+  `value` VARCHAR(45) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`));
